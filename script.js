41a666bcb443e8526606cc2a4ba10e742eed42d9
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-09-21 21:16:31 -0700

    working prompts

--- script.js
+++ script.js
@@ -9,7 +9,42 @@
 // * You can create other functions that are called from within
 //   generatePassword
 //
-// * Gather user input with prompt's and confirm's
+// * Gather user input with         prompt's and confirm's
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+
+function generatePassword(){
+  var password = " ";
+  var length = prompt("How many characters? Min 8, Max 128");
+  if(length <8 || length >128) {
+    generatePassword()
+  }
+  var uppercase = confirm("Include Upper case Letters?");
+  var lowercase = confirm("Include Lower case Letters?");
+  var numbers = confirm("Include Numbers?");
+  var specialchar = confirm("Include Special Characters?");
+  if(uppercase === true) {
+    var uppercaseLetters = ["A","B","C","D","F","G"   ]
+    var ramdomUppercase = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length) ]
+    console.log(ramdomUppercase)
+  }
+
+}
+
 
 ///////////////////////////////////////////////////////////////////////
 // DO NOT TOUCH THIS CODE
